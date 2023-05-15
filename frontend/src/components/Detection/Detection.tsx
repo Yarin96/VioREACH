58937ebef4f593f258282f194c8b38ac2dd962ce
Yarin96 <yarin0008@gmail.com>
2023-05-15 14:53:01 +0300

    add major changes to detection page, add metadata information from instagrap api

--- frontend/src/components/Detection/Detection.tsx
+++ frontend/src/components/Detection/Detection.tsx
@@ -1,20 +1,88 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Detection.css";
-import Button from "../../shared/components/UIElements/Button/Button";
 import Instructions from "./Instructions";
 import MainContainer from "../../shared/components/Container/MainContainer";
+import {
+  Typography,
+  Container,
+  Divider,
+  Button,
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  CardActionArea,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ImageIcon from "@mui/icons-material/Image";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import DynamicFeedIcon from "@mui/icons-material/DynamicFeed";
+
+interface PostsInfo {
+  username: string;
+  videoUrls: string[];
+  imageUrls: string[];
+}
 
 const Detection: React.FC = () => {
   const [result, setResult] = useState(null);
+  const [arePostsExtracted, setArePostsExtracted] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
-  const sendDataToServer = async (videos: any) => {
+  const [postsInfo, setPostsInfo] = useState<PostsInfo>({
+    username: "default",
+    videoUrls: [],
+    imageUrls: [],
+  });
+
+  const extractPostsHandler = async (event: any) => {
+    const response: any = await axios.get(
+      "https://localhost:8443/detection/posts"
+    );
+
+    setArePostsExtracted(true);
+    // console.log(response.data.fetchedData);
+    setPostsInfo((prevPostsInfo) => {
+      return {
+        ...prevPostsInfo,
+        username: response.data.fetchedData.data[0].username,
+        videoUrls: response.data.fetchedData.data
+          .filter((item: any) => item.media_type === "VIDEO")
+          .map((item: any) => item.media_url),
+        imageUrls: response.data.fetchedData.data
+          .filter((item: any) => item.media_type === "IMAGE")
+          .map((item: any) => item.media_url),
+      };
+    });
+  };
+
+  const openNewWindow = (url: string, width: number, height: number) => {
+    const left = window.screen.width / 2 - width / 2;
+    const top = window.screen.height / 2 - height / 2;
+    const features = `left=${left},top=${top},width=${width},height=${height}`;
+    window.open(url, "_blank", features)?.focus();
+  };
+
+  const getInstagramAccessHandler = async () => {
+    try {
+      setIsClicked(true);
+      const appId = process.env.REACT_APP_APP_ID_KEY;
+      const redirectUri = process.env.REACT_APP_REDIRECT_URI;
+      let url = `https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
+      openNewWindow(url, 600, 600);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendDataToServer = async () => {
     try {
       const tempToken = localStorage.getItem("token");
       const response: any = await axios.post(
         "http://127.0.0.1:5000/detection",
         {
-          video_url: videos[1],
+          video_url: postsInfo.videoUrls[0],
         },
         {
           headers: {
@@ -29,32 +97,144 @@ const Detection: React.FC = () => {
     }
   };
 
-  const openNewWindow = (url: string, width: number, height: number) => {
-    const left = window.screen.width / 2 - width / 2;
-    const top = window.screen.height / 2 - height / 2;
-    const features = `left=${left},top=${top},width=${width},height=${height}`;
-    const authWindow = window.open(url, "_blank", features)?.focus();
-
-    return authWindow;
-  };
-
-  const getInstagramAccessHandler = async () => {
-    try {
-      const appId = process.env.REACT_APP_APP_ID_KEY;
-      const redirectUri = process.env.REACT_APP_REDIRECT_URI;
-      let url = `https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
-      openNewWindow(url, 600, 600);
-      const response: any = await axios.get("https://localhost:8443/detection");
-      // sendDataToServer(response.data.videoList);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <MainContainer>
-      <Instructions />
-      <Button text="Permit Access" onClick={getInstagramAccessHandler} />
+      <Container style={{ marginTop: "16px", marginBottom: "56px" }}>
+        <Typography
+          fontWeight="bold"
+          variant="h2"
+          style={{ marginTop: "36px", marginBottom: "26px" }}
+          fontFamily={"'Rubik', sans-serif"}
+        >
+          Detection Scanner 🔍
+        </Typography>
+        <Divider />
+        {!isClicked && !arePostsExtracted && (
+          <Instructions onAccessClick={getInstagramAccessHandler} />
+        )}
+        {isClicked && !arePostsExtracted && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              mt: "26px",
+              maxWidth: 400,
+              mx: "auto",
+            }}
+          >
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ mt: "12px" }}
+              onClick={extractPostsHandler}
+            >
+              Extract Posts
+            </Button>
+          </Box>
+        )}
+        {isClicked && arePostsExtracted && (
+          <>
+            <Typography
+              fontWeight="bold"
+              variant="h5"
+              style={{
+                marginTop: "36px",
+                marginBottom: "86px",
+                textDecoration: "underline",
+              }}
+              fontFamily={"'Rubik', sans-serif"}
+            >
+              Profile History Stats
+            </Typography>
+            <Grid container spacing={20} style={{ marginBottom: "56px" }}>
+              <Grid item xs={12} md={3}>
+                <Card sx={{ bgcolor: "#F1B000", color: "#333" }}>
+                  <CardActionArea>
+                    <AccountCircleIcon style={{ fontSize: 80 }} />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        Username
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary">
+                        {postsInfo.username}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Card sx={{ bgcolor: "#319938", color: "#333" }}>
+                  <CardActionArea>
+                    <ImageIcon style={{ fontSize: 80 }} />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        No. of Images
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary">
+                        {postsInfo.imageUrls.length}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Card sx={{ bgcolor: "#00adec", color: "#333" }}>
+                  <CardActionArea>
+                    <OndemandVideoIcon style={{ fontSize: 80 }} />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        No. of Videos
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary">
+                        {postsInfo.videoUrls.length}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Card sx={{ bgcolor: "#b2ec73", color: "#333" }}>
+                  <CardActionArea>
+                    <DynamicFeedIcon style={{ fontSize: 80 }} />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        Total Posts
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary">
+                        {postsInfo.videoUrls.length +
+                          postsInfo.imageUrls.length}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            </Grid>
+            <Divider />
+            <Typography
+              fontWeight="bold"
+              variant="h5"
+              style={{
+                marginTop: "36px",
+                marginBottom: "36px",
+                textDecoration: "underline",
+              }}
+              fontFamily={"'Rubik', sans-serif"}
+            >
+              Activate Our Algorithm
+            </Typography>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ mt: "12px" }}
+              onClick={sendDataToServer}
+            >
+              Start Scanning Your Profile
+            </Button>
+          </>
+        )}
+      </Container>
     </MainContainer>
   );
 };
--- frontend/src/components/Detection/Instructions.tsx
+++ frontend/src/components/Detection/Instructions.tsx
@@ -1,19 +1,141 @@
-import { Typography, Divider, Container } from "@mui/material";
+import { useState } from "react";
+import {
+  Box,
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  Button,
+  Paper,
+  Typography,
+  Divider,
+} from "@mui/material";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
-const Instructions = () => {
+const steps = [
+  {
+    label: "Grant Access to Instagram account",
+    description: `First, you'd be asked to grant access to your Instagram account so we could access your personal media.
+    A pop-up window will appear with Instagram Account Authorization request. 
+    Kindly log-in to your account and click "Allow".`,
+  },
+  {
+    label: "Disable 'Bad URL' warnings",
+    description:
+      "As Instagram verifies your credentials, you might encounter browser warnings regarding bad URL as security vulnerability, please ignore them and accept permissions.",
+  },
+  {
+    label: "Auth Succeeded!",
+    description: `You should be able to see a 'Authentication Succeeded' message. 
+    Close the pop-up window, and a 'Extract Posts' button should be visible. 
+    Click it in order to start scanning your account posts.`,
+  },
+  {
+    label: "Activate Algorithm",
+    description: `After the posts were extracted, you should be able to see the user's uploads history stats. A new button will be shown to start the detection process.`,
+  },
+];
+
+export default function Instructions(props: any) {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
   return (
-    <Container style={{ marginTop: "26px" }}>
-      <Typography variant="h4" style={{ marginBottom: "16px" }}>
-        The Violence Detection Scanner!
-      </Typography>
+    <>
       <Divider />
-      <Typography variant="body1" style={{ marginTop: "16px" }}>
-        First, log in to your Instagram account and grant user permissions.
-        Then, click the button to start scanning your social media account using
-        our algorithm.
+      <Typography
+        variant="body1"
+        style={{ marginTop: "16px", fontSize: "24px", fontWeight: "bold" }}
+      >
+        - Instructions - <br />
+        <ArrowDownwardIcon />
       </Typography>
-    </Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: 400,
+          height: "400px",
+          mx: "auto",
+          mt: "1rem",
+          bgcolor: "#ffffff",
+          padding: "32px",
+          border: "1px solid black",
+          borderRadius: "8px",
+        }}
+      >
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((step, index) => (
+            <Step key={step.label}>
+              <StepLabel
+                optional={
+                  index === 3 ? (
+                    <Typography variant="caption">Last step</Typography>
+                  ) : null
+                }
+              >
+                {step.label}
+              </StepLabel>
+              <StepContent>
+                <Typography>{step.description}</Typography>
+                <Box sx={{ mb: 2 }}>
+                  <div>
+                    <Button
+                      variant="contained"
+                      onClick={handleNext}
+                      sx={{ mt: 1, mr: 1 }}
+                    >
+                      {index === steps.length - 1 ? "Finish" : "Continue"}
+                    </Button>
+                    <Button
+                      disabled={index === 0}
+                      onClick={handleBack}
+                      sx={{ mt: 1, mr: 1 }}
+                    >
+                      Back
+                    </Button>
+                  </div>
+                </Box>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === steps.length && (
+          <Paper
+            square
+            elevation={0}
+            sx={{
+              backgroundColor: "#f5f5f5",
+              border: "1px solid #ccc",
+              borderRadius: "8px",
+              p: 3,
+            }}
+          >
+            <Typography>We are all set! 👌</Typography>
+            <Button
+              variant="contained"
+              onClick={props.onAccessClick}
+              sx={{ mt: 1, mr: 1 }}
+            >
+              Permit Instagram Access
+            </Button>
+            <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+              Reset Instructions
+            </Button>
+          </Paper>
+        )}
+      </Box>
+    </>
   );
-};
-
-export default Instructions;
+}
